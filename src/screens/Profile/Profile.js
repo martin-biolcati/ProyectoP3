c2f411c16bd2b8e2c3fe70f9efceecffbc1d1c29
martin-biolcati <martinbiolcati@gmail.com>
2023-11-13 15:31:57 -0300

    profile

--- src/screens/Profile/Profile.js
+++ src/screens/Profile/Profile.js
@@ -1,3 +1,68 @@
-import react, {Component} from 'react';
+import react, { Component } from 'react';
+import { db, auth } from '../../firebase/config';
 import {TextInput, TouchableOpacity, View, Text, StyleSheet} from 'react-native';
-import {auth} from '../../firebase/config';
+
+class Profile extends Component {
+    constructor(){
+        super()
+        this.state={
+        }
+    }
+
+    componentDidMount(){
+         }
+         
+    logout(){
+            auth.signOut();
+            this.props.navigation.navigate('Login')
+        }
+
+    render(){
+        return(
+            <View style={styles.formContainer}>
+                <TouchableOpacity onPress={ () => this.props.navigation.navigate('Home')}>
+                <Text>HOME</Text>
+                </TouchableOpacity>
+                <TouchableOpacity onPress={()=>this.logout()}>
+                    <Text>Logout</Text>
+                </TouchableOpacity>
+                
+                <Text>Chai BOT</Text>
+            </View>
+        )
+    }
+}
+
+const styles = StyleSheet.create({
+    formContainer:{
+        paddingHorizontal:600,
+        marginTop: 20,
+    },
+    input:{
+        height:20,
+        paddingVertical:15,
+        paddingHorizontal: 10,
+        borderWidth:1,
+        borderColor: '#ccc',
+        borderStyle: 'solid',
+        borderRadius: 6,
+        marginVertical:10,
+    },
+    button:{
+        backgroundColor:'#28a745',
+        paddingHorizontal: 10,
+        paddingVertical: 6,
+        textAlign: 'center',
+        borderRadius:4, 
+        borderWidth:1,
+        borderStyle: 'solid',
+        borderColor: '#28a745'
+    },
+    textButton:{
+        color: '#fff'
+    }
+
+})
+
+
+export default Profile;
